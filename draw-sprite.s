a1e97460c823feb5db2a16ea1e2696e2fd2df13e
Anshil Gandhi <gandhi21299@gmail.com>
2022-08-24 17:45:51 -0600

    loading sprites in progress

--- draw-sprite.s
+++ draw-sprite.s
@@ -1,101 +1,166 @@
-.segment "HEADER"
-  ; .byte "NES", $1A      ; iNES header identifier
-  .byte $4E, $45, $53, $1A
-  .byte 2               ; 2x 16KB PRG code
-  .byte 1               ; 1x  8KB CHR data
-  .byte $01, $00        ; mapper 0, vertical mirroring
 
-.segment "VECTORS"
-  ;; When an NMI happens (once per frame if enabled) the label nmi:
-  .addr nmi
-  ;; When the processor first turns on or is reset, it will jump to the label reset:
-  .addr reset
-  ;; External interrupt IRQ (unused)
-  .addr 0
+; +-----------------+
+; | Memory map      |
+; +-----------------+
+;
+; $0000 - $0800 : Internal RAM, 2KB chip in the NES
+; $2000 - $2007 : PPU access ports
+;   $2000 - PPUCTRL
+;   $2001 - PPUMASK
+;   $2002 - PPUSTATUS
+;   $2003 - OAMADDR
+;   $2004 - OAMDATA
+;   $2005 - PPUSCROLL
+;   $2006 - PPUADDR
+;   $2007 - PPUDATA
+;
+; $4000 - $4017 : Audio and controller access ports
+; $6000 - $7FFF : Optional WRAM inside the game cart
+; $8000 - $FFFF : Game cart ROM
+;
+; +-----------------+
+; | iNES Header     |
+; +-----------------+
+;
+; 16 byte iNES header gives the emulator all the
+; information about the game including mapper,
+; graphics mirroring, and PRG/CHR sizes. These
+; can be included inside the asm at the very
+; beginning.
 
-; "nes" linker config requires a STARTUP section, even if it's empty
-.segment "STARTUP"
+  .inesprg 1    ; 1 x 16KB PRG code
+  .ineschr 1    ; 1 x 8KB CHR data
+  .inesmap 0    ; mapper 0 = NROM, no bank swapping
+  .inesmir 1    ; background mirroring enabled
 
-; Main code segement for the program
-.segment "CODE"
-
-reset:
-  sei		; disable IRQs
-  cld		; disable decimal mode
+; +----------------------+
+; | NES has powered on   |
+; +----------------------+
+;
+  .bank 0
+  .org $C000    ; Tells the assembler where to start this bank
+RESET:
+  sei           ; disable IRQs
+  cld           ; disable decimal mode, meant to make decimal
+                ; arithmetic "easier"
   ldx #$40
-  stx $4017	; disable APU frame IRQ
-  ldx #$ff 	; Set up stack
-  txs		;  .
-  inx		; now X = 0
-  stx $2000	; disable NMI
-  stx $2001 	; disable rendering
-  stx $4010 	; disable DMC IRQs
+  stx $4017     ; disable APU frame IRQ
+  ldx #$ff
+  txs           ; set up stack
+  inx           ; now X = 0
+  stx $2000     ; disable NMI
+  stx $2001     ; disable rendering
+  stx $4010     ; disable DMC IRQs
 
-;; first wait for vblank to make sure PPU is ready
 vblankwait1:
   bit $2002
   bpl vblankwait1
 
-clear_memory:
+; +----------------+
+; | Clear memory   |
+; +----------------+
+;
+clrmem:
   lda #$00
   sta $0000, x
   sta $0100, x
-  sta $0200, x
   sta $0300, x
   sta $0400, x
   sta $0500, x
   sta $0600, x
   sta $0700, x
+  lda #$fe
+  sta $0200, x  ; move all sprites off screen
   inx
-  bne clear_memory
+  bne clrmem
 
-;; second wait for vblank, PPU is ready after this
-vblankwait2:
+vblankwait2:    ; second wait for vblank, PPU is ready after this
   bit $2002
   bpl vblankwait2
 
-main:
-load_palettes:
-  lda $2002       ; Read PPU status to reset the high/low latch
-  lda #$3f
-  sta $2006       ; Write the high bits of value at $3f00
+; +-----------------------+
+; | Load game palettes    |
+; +-----------------------+
+;
+LoadPalettes:
+  lda $2002     ; read PPU status to reset high/low latch
+  lda #$3f      ; max out 0011 1111
+  sta $2006
   lda #$00
-  sta $2006       ; Write the low bits of value at $3f00
-  ldx #$00
+  sta $2006
+  ldx #$00      ; start at 0
 
-@loop:
-  lda palettes, x
+LoadPalettesLoop:
+  lda palette, x
   sta $2007
   inx
   cpx #$20
-  bne @loop
+  bne LoadPalettesLoop
 
-enable_rendering:
-  lda #%10000000	; Enable NMI
-  sta $2000
-  lda #%00010000	; Enable Sprites
-  sta $2001
+MainLoop:
+  lda #$00
+  sta $2003     ; clear the low byte of the RAM address
+  lda #$02
+  sta $4014     ; set the high byte of the RAM address,
+                ; start the transfer
 
-nmi:
-  ldx #$00 	; Set SPR-RAM address to 0
-  stx $2003
-@loop:	;lda hello, x 	; Load the hello message into SPR-RAM
-  sta $2004
-  inx
-  cpx #$24
-  bne @loop
-  rti
+  jsr Draw
 
-palettes:
-  .byte $22, $29, $1A, $0F
-  .byte $22, $36, $17, $0F
-  .byte $22, $30, $21, $0F
-  .byte $22, $27, $17, $0F
-  .byte $22, $16, $27, $18
-  .byte $22, $1A, $30, $27
-  .byte $22, $16, $30, $27
-  .byte $00, $0F, $36, $17    ; Sprite
+  jsr Update
 
-.segment "CHARS"
+  rti           ; return from interrupt
 
-.incbin "mario.chr"
+; +-----------------------+
+; | Data initialization   |
+; +-----------------------+
+; .db command is a macro for storing bytes
+; in memory without having to write
+  .bank 1
+  .org $e000
+palette:
+    ;; Background Palletes (0-3)
+  .db $08,$1A,$38,$18, $08,$02,$38,$3C, $08,$1C,$15,$14, $08,$02,$38,$2A
+  ;;  Character Palletes (0-3)
+  .db $21,$2C,$11,$15, $0F,$35,$36,$37, $0F,$39,$3A,$3B, $0F,$3D,$3E,$0F
+
+
+
+; Define interrupt vectors at the top of memory $FFFF
+; Basically registering callbacks for different functions
+; NMI, RESET and IRQ.
+;
+; .org means starting at $FFFA
+; .dw means store dataword
+;   stores in little endian order (least significant bit first)
+
+  .bank 1
+  .org $FFFA    ; first of the three vectors starts here
+nescallback:
+  .dw MainLoop  ; when an NMI happens, jump to label NMI
+  .dw Reset     ; when the processor is reset, jump to Reset
+  .dw 0         ; external interrupt IRQ for audio
+
+; Load in external sprites or audio data here
+  .bank 2
+  .org $0000
+  .incbin "mario.chr"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
