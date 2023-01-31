15ebf55a31ad86541332146f01459f709b8103d7
Anshil Gandhi <gandhi21299@gmail.com>
2023-01-30 22:53:58 -0700

    [bouncing-toad] Draw toad

--- bouncing-toad.s
+++ bouncing-toad.s
@@ -1,9 +1,72 @@
+; PPU control register
+; 7  bit  0
+; ---- ----
+; VPHB SINN
+; |||| ||||
+; |||| ||++- Base nametable address
+; |||| ||    (0 = $2000; 1 = $2400; 2 = $2800; 3 = $2C00)
+; |||| |+--- VRAM address increment per CPU read/write of PPUDATA
+; |||| |     (0: add 1, going across; 1: add 32, going down)
+; |||| +---- Sprite pattern table address for 8x8 sprites
+; ||||       (0: $0000; 1: $1000; ignored in 8x16 mode)
+; |||+------ Background pattern table address (0: $0000; 1: $1000)
+; ||+------- Sprite size (0: 8x8 pixels; 1: 8x16 pixels – see PPU OAM#Byte 1)
+; |+-------- PPU master/slave select
+; |          (0: read backdrop from EXT pins; 1: output color on EXT pins)
+; +--------- Generate an NMI at the start of the
+;            vertical blanking interval (0: off; 1: on)
 PPUCTRL   = $2000
+
+; PPU Mask register: controls the rendering of sprites
+; 7  bit  0
+; ---- ----
+; BGRs bMmG
+; |||| ||||
+; |||| |||+- Greyscale (0: normal color, 1: produce a greyscale display)
+; |||| ||+-- 1: Show background in leftmost 8 pixels of screen, 0: Hide
+; |||| |+--- 1: Show sprites in leftmost 8 pixels of screen, 0: Hide
+; |||| +---- 1: Show background
+; |||+------ 1: Show sprites
+; ||+------- Emphasize red (green on PAL/Dendy)
+; |+-------- Emphasize green (red on PAL/Dendy)
+; +--------- Emphasize blue
 PPUMASK   = $2001
+
+; PPU Status register: reflects the state of various functions inside the PPU
+; 7  bit  0
+; ---- ----
+; VSO. ....
+; |||| ||||
+; |||+-++++- PPU open bus. Returns stale PPU bus contents.
+; ||+------- Sprite overflow. The intent was for this flag to be set
+; ||         whenever more than eight sprites appear on a scanline, but a
+; ||         hardware bug causes the actual behavior to be more complicated
+; ||         and generate false positives as well as false negatives; see
+; ||         PPU sprite evaluation. This flag is set during sprite
+; ||         evaluation and cleared at dot 1 (the second dot) of the
+; ||         pre-render line.
+; |+-------- Sprite 0 Hit.  Set when a nonzero pixel of sprite 0 overlaps
+; |          a nonzero background pixel; cleared at dot 1 of the pre-render
+; |          line.  Used for raster timing.
+; +--------- Vertical blank has started (0: not in vblank; 1: in vblank).
+;            Set at dot 1 of line 241 (the line *after* the post-render
+;            line); cleared after reading $2002 and at dot 1 of the
+;            pre-render line.
 PPUSTATUS = $2002
-PPUADDR   = $2006
-PPUDATA   = $2007
+
+; Object Attribute Memory address: write the address of OAM to be accessed here.
 OAMADDR   = $2003
+
+; PPU scrolling position register: used to change the scroll position
+PPUSCROLL = $2005
+
+; PPU address register
+PPUADDR   = $2006
+
+; PPU data port register: VRAM read/write data register
+PPUDATA   = $2007
+
+; OAM data register
 OAMDMA    = $4014
 
 .segment "HEADER"
@@ -42,6 +105,29 @@ vblankwait:
 
 .proc main
     ;; Prepare PPU to load a palette
+    ldx PPUSTATUS
+    ldx #$3f
+    stx PPUADDR
+    ldx #$00
+    stx PPUADDR
+
+    ;; Load all 8 4-color palettes into PPU
+    ldx #$00
+LoadPalette:
+    lda PaletteData, x
+    sta PPUDATA
+    inx
+    cpx #$20
+    bne LoadPalette
+
+    ;; Load toad sprite into PPU
+    ldx $00
+LoadToadSprite:
+    lda ToadSpriteData, x
+    sta $0200, x
+    inx
+    cpx $10
+    bne LoadToadSprite
 
 vblankwait:       ; wait for another vblank before continuing
     bit PPUSTATUS
@@ -55,6 +141,20 @@ vblankwait:       ; wait for another vblank before continuing
 forever:
     jmp forever
 .endproc
+
+;;; Palette Data goes here ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+PaletteData:
+    .byte $00, $0F, $00, $10, 	$00, $0A, $15, $01, 	$00, $29, $28, $27, 	$00, $34, $24, $14 	;background palettes
+	.byte $31, $0F, $15, $30, 	$00, $0F, $11, $30, 	$00, $0F, $30, $27, 	$00, $3C, $2C, $1C 	;sprite palettes
+;;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+;;; Sprite Data goes here ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ToadSpriteData:
+    .byte $40, $00, $00, $40
+    .byte $40, $01, $00, $48
+    .byte $48, $10, $00, $40
+    .byte $48, $11, $00, $48
+;;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 .segment "VECTORS"
 .addr nmi_handler, reset_handler, irq_handler
